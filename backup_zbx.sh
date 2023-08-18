1c9c36fe95bac057f5138b666b25afed926188b0
Joandson Bezerra <36518985+joandson19@users.noreply.github.com>
2023-08-18 14:13:28 -0300

    Update backup_zbx.sh

--- backup_zbx.sh
+++ backup_zbx.sh
@@ -2,6 +2,6 @@
 # ----Config ----
 ID_TELEGRAM='COLE SEU ID AQUI'
 # ---------------
-/etc/backups/zabbix-dump -o /tmp &>/dev/null
+sudo -u zabbix /etc/backups/zabbix-dump -o /tmp &>/dev/null
 /bin/telegram -f "${ID_TELEGRAM}" "/tmp/zabbix_cfg*.gz" "zabbix" "Backup zabbix"
 rm -f /tmp/*.gz
